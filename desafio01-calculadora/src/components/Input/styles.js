e80d31431a0fb96ca9c1927da7f0c437f893e384
= <=>
2025-03-15 22:42:04 -0300

    criando botoes

--- desafio01-calculadora/src/components/Input/styles.js
+++ desafio01-calculadora/src/components/Input/styles.js
@@ -1,1 +1,30 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 85px;
+    background-color:rgb(179, 131, 242);
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size:  24px;
+    font-family: 'Roboto';
+
+    border-radius: 10px;
+
+    input {
+        width: 100%;
+        height: 85px;
+        background-color:rgb(179, 131, 242);
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size:  24px;
+        font-family: 'Roboto';
+        color: white;
+    }
+`
