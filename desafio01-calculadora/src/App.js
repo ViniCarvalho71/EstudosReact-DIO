9518cce17baa93f61bda820c3a84f2ea6302d036
= <=>
2025-03-19 15:06:06 -0300

    projeto replicado

--- desafio01-calculadora/src/App.js
+++ desafio01-calculadora/src/App.js
@@ -5,37 +5,83 @@ import { useState } from 'react';
 
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState("0");
+  const [firstNumber, setFirstNumber] = useState("0");
+  const [operation, setOperation] = useState('');
+
+  const handleOnClear = () => {
+        setCurrentNumber('0')
+        setFirstNumber('0')
+        setOperation('')
+  }
 
   const handleAddNumber = (number) => {
+        setCurrentNumber(prev => `${prev == 0?'':prev}${number}`)
+  }
 
+  const handleSum = () => {
+      if(firstNumber == 0){
+        setFirstNumber(String(currentNumber))
+        setCurrentNumber('0') 
+        setOperation("+")
+      } else {
+        const sum = Number(firstNumber) + Number(currentNumber)
+        setCurrentNumber(String(sum))
+      }
+  }
+
+  const handleSub = () => {
+    if(firstNumber == 0){
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0') 
+      setOperation("-")
+    } else {
+      const sub = Number(firstNumber) - Number(currentNumber)
+      setCurrentNumber(String(sub))
+    }
+}
+
+  const handleEquals = () => {
+    if(!firstNumber == 0 && operation != '' && currentNumber != 0){
+      switch(operation){
+        case '+':
+          handleSum();
+          break;
+        case '-':
+          handleSub();
+          break;
+          default:
+          break;
+      }
+    } 
+    
   }
   return (
     <Container>
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="*"/>
-          <Button label="/"/>
-          <Button label="C"/>
-          <Button label="X"/>
+          <Button label="x" onClick={() => handleAddNumber('x')}/>
+          <Button label="/" onClick={() => handleAddNumber('/')}/>
+          <Button label="C" onClick={() => handleOnClear()}/>
+          <Button label="X" onClick={() => handleAddNumber('X')}/>
         </Row>
         <Row>
-          <Button label="7"/>
-          <Button label="8"/>
-          <Button label="9"/>
-          <Button label="-"/>
+          <Button label="7" onClick={() => handleAddNumber('7')}/>
+          <Button label="8" onClick={() => handleAddNumber('8')}/>
+          <Button label="9" onClick={() => handleAddNumber('9')}/>
+          <Button label="-" onClick={handleSub}/>
         </Row>
         <Row>
-          <Button label="4"/>
-          <Button label="5"/>
-          <Button label="6"/>
-          <Button label="+"/>
+          <Button label="4" onClick={() => handleAddNumber('4')}/>
+          <Button label="5" onClick={() => handleAddNumber('5')}/>
+          <Button label="6" onClick={() => handleAddNumber('6')}/>
+          <Button label="+" onClick={handleSum}/>
         </Row>
         <Row>
-          <Button label="1"/>
-          <Button label="2"/>
-          <Button label="3"/>
-          <Button label="="/>
+          <Button label="1" onClick={() => handleAddNumber('1')}/>
+          <Button label="2" onClick={() => handleAddNumber('2')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="=" onClick={handleEquals}/>
         </Row>
       </Content>
     </Container>
